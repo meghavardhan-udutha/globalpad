1da43d17415844adad64822d10d99e64104f04b9
udutha-meghavardhan <meghavardhan10@gmail.com>
2026-05-22 19:33:21 +0530

    Integrated Cloudinary persistent storage

--- globalpad/settings.py
+++ globalpad/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-globalpad-secret-key-change-in-production'
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
